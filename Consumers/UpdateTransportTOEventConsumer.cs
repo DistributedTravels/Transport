d8b1a43171f8166f70447b65e743adccea21b6b1
Kacper Ochotny <trinerxd@gmail.com>
2022-05-29 18:30:43 +0200

    mock class for async change handling

---
+++ Consumers/UpdateTransportTOEventConsumer.cs
@@ -0,0 +1,46 @@
+﻿using MassTransit;
+using Models.Transport;
+using Models.Transport.Dto;
+using Transport.Database;
+using Transport.Database.Tables;
+
+namespace Transport.Consumers
+{
+    public class UpdateTransportTOEventConsumer : IConsumer<UpdateTransportTOEvent>
+    {
+        public async Task Consume(ConsumeContext<UpdateTransportTOEvent> context)
+        {
+            var @event = context.Message;
+
+            switch (@event.Table)
+            {
+                case UpdateTransportTOEvent.Tables.DESTINATION:
+                    await HandleDestinationUpdate((DestinationChangeDto)@event.Details);
+                    break;
+                case UpdateTransportTOEvent.Tables.SOURCE:
+                    await HandleSourceUpdate((SourceChangeDto)@event.Details);
+                    break;
+                case UpdateTransportTOEvent.Tables.TRAVEL:
+                    await HandleTravelUpdate((TravelChangeDto)@event.Details);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public async Task HandleDestinationUpdate(DestinationChangeDto dcd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task HandleSourceUpdate(SourceChangeDto dcd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task HandleTravelUpdate(TravelChangeDto dcd)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
