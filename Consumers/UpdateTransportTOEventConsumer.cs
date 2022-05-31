c35c446e82ac246586c42b55187a390630e621a7
Kacper Ochotny <trinerxd@gmail.com>
2022-05-31 22:58:34 +0200

    tested updates

--- Consumers/UpdateTransportTOEventConsumer.cs
+++ Consumers/UpdateTransportTOEventConsumer.cs
@@ -3,6 +3,7 @@ using Models.Transport;
 using Models.Transport.Dto;
 using Transport.Database;
 using Transport.Database.Tables;
+using Newtonsoft.Json;
 
 namespace Transport.Consumers
 {
@@ -15,13 +16,13 @@ namespace Transport.Consumers
             switch (@event.Table)
             {
                 case UpdateTransportTOEvent.Tables.DESTINATION:
-                    await HandleDestinationUpdate((DestinationChangeDto)@event.Details, @event.Action);
+                    await HandleDestinationUpdate(@event.DestinationDetails, @event.Action);
                     break;
                 case UpdateTransportTOEvent.Tables.SOURCE:
-                    await HandleSourceUpdate((SourceChangeDto)@event.Details, @event.Action);
+                    await HandleSourceUpdate(@event.SourceDetails, @event.Action);
                     break;
                 case UpdateTransportTOEvent.Tables.TRAVEL:
-                    await HandleTravelUpdate((TravelChangeDto)@event.Details, @event.Action);
+                    await HandleTravelUpdate(@event.TravelDetails, @event.Action);
                     break;
                 default:
                     break;
@@ -36,20 +37,27 @@ namespace Transport.Consumers
                 switch (action)
                 {
                     case UpdateTransportTOEvent.Actions.NEW:
-                        if (!dest.Any())
+                        if (!dest.Any() && dcd.Name.Length > 0 && dcd.Distance >= 0)
                             context.Add(new Destination { Name = dcd.Name, Distance = dcd.Distance });
                         break;
                     case UpdateTransportTOEvent.Actions.UPDATE:
                         if (dest.Any())
-                            dest.Single().Distance = dcd.Distance;
+                        {
+                            var single = dest.Single();
+                            single.Distance = dcd.Distance >= 0 ? dcd.Distance : single.Distance;
+                            single.Name = dcd.Name.Length > 0 ? dcd.NewName : single.Name;
+                        }
                         break;
                     case UpdateTransportTOEvent.Actions.DELETE:
                         if (dest.Any())
-                            context.Remove(dest);
+                            context.Remove(dest.Single());
                         break;
                     default:
                         break;
                 }
+                if (context.ChangeTracker.HasChanges())
+                    Console.WriteLine($"Transport: Change {action} in table DESTINATION");
+
                 await context.SaveChangesAsync();
             }
         }
@@ -62,21 +70,23 @@ namespace Transport.Consumers
                 switch (action)
                 {
                     case UpdateTransportTOEvent.Actions.NEW:
-                        if (!src.Any())
+                        if (!src.Any() && scd.Name.Length > 0)
                             context.Add(new Source { Name = scd.Name });
                         break;
                     case UpdateTransportTOEvent.Actions.UPDATE:
-                        // useless atm, need to redesign SourceChangeDto
-                        if (src.Any())
-                            src.Single().Name = scd.Name;
+                        if (src.Any() && scd.NewName.Length > 0)
+                            src.Single().Name = scd.NewName;
                         break;
                     case UpdateTransportTOEvent.Actions.DELETE:
                         if (src.Any())
-                            context.Remove(src);
+                            context.Remove(src.Single());
                         break;
                     default:
                         break;
                 }
+                if (context.ChangeTracker.HasChanges())
+                    Console.WriteLine($"Transport: Change {action} in table SOURCE");
+
                 await context.SaveChangesAsync();
             }
         }
@@ -90,26 +100,30 @@ namespace Transport.Consumers
                 {
                     case UpdateTransportTOEvent.Actions.NEW:
                         if (!trvl.Any())
-                            context.Add(new Travel { DepartureTime = tcd.DepartureTime.ToUniversalTime(), Source = tcd.Source, Destination = tcd.Destination, FreeSeats = tcd.AvailableSeats, Price = tcd.Price });
+                            if(tcd.Destination.Length > 0 && tcd.Source.Length > 0 && tcd.AvailableSeats >= 0 && tcd.DepartureTime != DateTime.UnixEpoch && tcd.Price > 0)
+                                context.Add(new Travel { DepartureTime = tcd.DepartureTime.ToUniversalTime(), Source = tcd.Source, Destination = tcd.Destination, FreeSeats = tcd.AvailableSeats, Price = tcd.Price });
                         break;
                     case UpdateTransportTOEvent.Actions.UPDATE:
                         if (trvl.Any())
                         {
                             var utrvl = trvl.Single();
-                            utrvl.Destination = tcd.Destination;
-                            utrvl.Source = tcd.Source;
-                            utrvl.Price = tcd.Price;
-                            utrvl.DepartureTime = tcd.DepartureTime.ToUniversalTime();
-                            utrvl.FreeSeats = tcd.AvailableSeats;
+                            utrvl.Destination = tcd.Destination.Length > 0 ? tcd.Destination : utrvl.Destination;
+                            utrvl.Source = tcd.Source.Length > 0 ? tcd.Source : utrvl.Source;
+                            utrvl.Price = tcd.Price > 0 ? tcd.Price : utrvl.Price;
+                            utrvl.DepartureTime = tcd.DepartureTime != DateTime.UnixEpoch ? tcd.DepartureTime.ToUniversalTime() : utrvl.DepartureTime;
+                            utrvl.FreeSeats = tcd.AvailableSeats > 0 ? tcd.AvailableSeats : utrvl.FreeSeats;
                         }
                         break;
                     case UpdateTransportTOEvent.Actions.DELETE:
                         if (trvl.Any())
-                            context.Remove(trvl);
+                            context.Remove(trvl.Single());
                         break;
                     default:
                         break;
                 }
+                if (context.ChangeTracker.HasChanges())
+                    Console.WriteLine($"Transport: Change {action} in table TRAVEL");
+
                 await context.SaveChangesAsync();
             }
         }
--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using MassTransit;
 using Transport.Database;
 using Transport.Consumers;
 using Newtonsoft.Json;
+using Models.Transport;
+using Models.Transport.Dto;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -27,6 +29,7 @@ builder.Services.AddMassTransit(cfg =>
     cfg.AddConsumer<GetAvailableTravelsEventConsumer>();
     cfg.AddConsumer<ReserveTravelEventConsumer>();
     cfg.AddConsumer<UnreserveTravelEventConsumer>();
+    cfg.AddConsumer<UpdateTransportTOEventConsumer>();
 
     // telling masstransit to use rabbitmq
     cfg.UsingRabbitMq((context, rabbitCfg) =>
@@ -44,22 +47,118 @@ builder.Services.AddMassTransit(cfg =>
 
 var app = builder.Build();
 initDB();
-// bus for publishing a message, to check if everything works
-// THIS SHOULD NOT EXIST IN FINAL PROJECT
 
-/*var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
-{
-    cfg.Host("rabbitmq", "/", h =>
-    {
-        h.Username("guest");
-        h.Password("guest");
-    });
-});
-busControl.Start();
-await busControl.Publish(new GetAvailableDestinationsEvent());
-busControl.Stop();*/
+//testChanges();
 
 app.Run();
+
+/*async void testChanges()
+{
+    // bus for publishing a message, to check if everything works
+    // THIS SHOULD NOT EXIST IN FINAL PROJECT
+    await Task.Delay(15000);
+    var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
+    {
+        cfg.Host("rabbitmq", "/", h =>
+        {
+            h.Username("guest");
+            h.Password("guest");
+        });
+    });
+    busControl.Start();
+    /* testing Distance table */
+    /*await busControl.Publish(new UpdateTransportTOEvent() { 
+        Action = UpdateTransportTOEvent.Actions.NEW, 
+        Table = UpdateTransportTOEvent.Tables.DESTINATION,
+        DestinationDetails = new DestinationChangeDto { Name = "Abcd", Distance=2000 }
+    });
+    await Task.Delay(4000);
+    await busControl.Publish(new UpdateTransportTOEvent()
+    {
+        Action = UpdateTransportTOEvent.Actions.UPDATE,
+        Table = UpdateTransportTOEvent.Tables.DESTINATION,
+        DestinationDetails = new DestinationChangeDto { Name = "Abcd", NewName = "Efgh", Distance = 1000 }
+    });
+    await Task.Delay(4000);
+    await busControl.Publish(new UpdateTransportTOEvent()
+    {
+        Action = UpdateTransportTOEvent.Actions.DELETE,
+        Table = UpdateTransportTOEvent.Tables.DESTINATION,
+        DestinationDetails = new DestinationChangeDto { Name = "Abcd" }
+    });
+    await Task.Delay(4000);
+    await busControl.Publish(new UpdateTransportTOEvent()
+    {
+        Action = UpdateTransportTOEvent.Actions.DELETE,
+        Table = UpdateTransportTOEvent.Tables.DESTINATION,
+        DestinationDetails = new DestinationChangeDto { Name = "Efgh" }
+    });
+    /* testing Source table */
+    /*await busControl.Publish(new UpdateTransportTOEvent()
+    {
+        Action = UpdateTransportTOEvent.Actions.NEW,
+        Table = UpdateTransportTOEvent.Tables.SOURCE,
+        SourceDetails = new SourceChangeDto { Name = "Abcd"}
+    });
+    await Task.Delay(4000);
+    await busControl.Publish(new UpdateTransportTOEvent()
+    {
+        Action = UpdateTransportTOEvent.Actions.UPDATE,
+        Table = UpdateTransportTOEvent.Tables.SOURCE,
+        SourceDetails = new SourceChangeDto { Name = "Abcd", NewName = "Efgh"}
+    });
+    await Task.Delay(4000);
+    await busControl.Publish(new UpdateTransportTOEvent()
+    {
+        Action = UpdateTransportTOEvent.Actions.DELETE,
+        Table = UpdateTransportTOEvent.Tables.SOURCE,
+        SourceDetails = new SourceChangeDto { Name = "Abcd" }
+    });
+    await Task.Delay(4000);
+    await busControl.Publish(new UpdateTransportTOEvent()
+    {
+        Action = UpdateTransportTOEvent.Actions.DELETE,
+        Table = UpdateTransportTOEvent.Tables.SOURCE,
+        SourceDetails = new SourceChangeDto { Name = "Efgh" }
+    });
+    /* testing Travel table */
+    /*await busControl.Publish(new UpdateTransportTOEvent()
+    {
+        Action = UpdateTransportTOEvent.Actions.NEW,
+        Table = UpdateTransportTOEvent.Tables.TRAVEL,
+        TravelDetails = new TravelChangeDto { Source = "Abcd" }
+    });
+    await Task.Delay(4000);
+    await busControl.Publish(new UpdateTransportTOEvent()
+    {
+        Action = UpdateTransportTOEvent.Actions.NEW,
+        Table = UpdateTransportTOEvent.Tables.TRAVEL,
+        TravelDetails = new TravelChangeDto { Source = "Warszawa", Destination = "Hiszpania", AvailableSeats = 10, DepartureTime = DateTime.Now, Price = 1869.25 }
+    });
+    await Task.Delay(4000);
+    await busControl.Publish(new UpdateTransportTOEvent()
+    {
+        Action = UpdateTransportTOEvent.Actions.UPDATE,
+        Table = UpdateTransportTOEvent.Tables.TRAVEL,
+        TravelDetails = new TravelChangeDto { Id = 1, Price = 226.99 }
+    });
+    await Task.Delay(4000);
+    await busControl.Publish(new UpdateTransportTOEvent()
+    {
+        Action = UpdateTransportTOEvent.Actions.DELETE,
+        Table = UpdateTransportTOEvent.Tables.TRAVEL,
+        TravelDetails = new TravelChangeDto {}
+    });
+    await Task.Delay(4000);
+    await busControl.Publish(new UpdateTransportTOEvent()
+    {
+        Action = UpdateTransportTOEvent.Actions.DELETE,
+        Table = UpdateTransportTOEvent.Tables.TRAVEL,
+        TravelDetails = new TravelChangeDto { Id = 1, Source = "Efgh" }
+    });
+    /* end of tests */
+    /*busControl.Stop();
+}*/
 
 void initDB()
 {
